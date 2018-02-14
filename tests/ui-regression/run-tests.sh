8a7e64180b426119b44c4c36bc5fbd12fd2a8794
Julius Härtl <jus@bitgrid.net>
2018-02-14 16:28:10 +0100

    Wait for service
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/ui-regression/run-tests.sh
+++ tests/ui-regression/run-tests.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Waiting for Nextcloud servers to become available"
+until curl -I --silent http://acceptance-ui-php:8080 && curl -I --silent http://acceptance-ui-php:8081
+do
+    sleep 2
+done
+
 ./node_modules/.bin/mocha test/installSpec.js --timeout 10000
 ./node_modules/.bin/mocha test/loginSpec.js --timeout 10000
 ./node_modules/.bin/mocha test/publicSpec.js --timeout 5000
